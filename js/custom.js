f115d30a786f66bf17e9d5eda3a07341334d738a
Kunal Shantilal <KunalShantilal@Kunals-MacBook-Pro.local>
2017-04-28 18:57:50 +1200

    scraped hover

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
 
-	$(".maori-tiki-sleep").click(function(){
+	$(".maori-tiki").click(function(){
 		$('.english-text').toggle();
 		$('.maori-text').toggle();
-
-	});
-
-	$('.maori-tiki-sleep').hover(function(){
-		$('.maori-tiki').toggle();
 
 	});
 
